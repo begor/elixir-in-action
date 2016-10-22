8a97d882d331742920045e2229b6c2805d2b0259
Biryukov Egor <birykovegor@gmail.com>
2016-10-22 15:42:01 +0300

    Longest line length and longest line

--- streams.ex
+++ streams.ex
@@ -13,4 +13,23 @@ defmodule StreamsPractice do
     |> Enum.to_list
   end
 
+  def longest_line_length!(path) do
+    path
+    |> lines_length!
+    |> Enum.max
+  end
+
+  def longest_line!(path) do
+    path
+    |> File.stream!
+    |> Enum.reduce("", &line_bigger/2)
+  end
+
+  defp line_bigger(x, y) do
+    case String.length(x) > String.length(y) do
+      true -> x
+      false -> y
+    end
+  end
+
 end

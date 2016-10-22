8445cc624cdd5bd59af3221cd83b7f1d0a7ec906
Biryukov Egor <birykovegor@gmail.com>
2016-10-22 15:29:41 +0300

    Lines length

--- streams.ex
+++ streams.ex
@@ -1,9 +1,16 @@
 defmodule StreamsPractice do
-  def large_lines(path) do
+  def large_lines!(path) do
     path
     |> File.stream!
     |> Stream.map(&String.replace(&1, "\n", ""))
     |> Enum.filter(&(String.length(&1) > 80))
+  end
+
+  def lines_length!(path) do
+    path
+    |> File.stream!
+    |> Stream.map(&String.length/1)
+    |> Enum.to_list
   end
 
 end

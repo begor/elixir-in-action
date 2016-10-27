4d98e59c07a71694c646ab2d25e7785ef643f6b8
Egor Biriukov <birykovegor@gmail.com>
2016-10-27 18:10:24 +0300

    Interface functions.

--- keyvalue/server.ex
+++ keyvalue/server.ex
@@ -32,6 +32,14 @@ defmodule KeyValue do
   
   def init, do: HashDict.new
 
+  def put(pid, key, value) do
+    ServerProcess.call(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
+  end
+
   def handle_call({:put, key, value}, state) do
     {:ok, HashDict.put(state, key, value)}
   end
